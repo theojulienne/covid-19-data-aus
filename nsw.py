99818f7bfae8101b3936716ea5ae736ba4329e6c
Karla <karla.no.spam@gmail.com>
2020-03-23 15:39:32 +1100

    Scrape post list, rather than using RSS; add historical data

--- nsw.py
+++ nsw.py
@@ -1,40 +1,23 @@
+import collections
+import copy
 import datetime
 import json
 import re
 
 import bs4
 import requests
-import feedparser
 
 def main():
-  # Parse the NSW Health media RSS feed, pulling out
-  nsw_feed = feedparser.parse('https://www.health.nsw.gov.au/_layouts/feed.aspx?xsl=1&web=/news&page=4ac47e14-04a9-4016-b501-65a23280e841&wp=baabf81e-a904-44f1-8d59-5f6d56519965')
-  timeseries_data = get_timeseries_data(nsw_feed)
+  # The NSW Health RSS feed only goes back a few weeks, so we have to scrape this page instead :(
+  timeseries_data = get_timeseries_data('https://www.health.nsw.gov.au/news/Pages/2020-nsw-health.aspx')
+  timeseries_data = add_manual_data(timeseries_data)
 
+  # Muck with the data to get it into the format that's expected
   dates = sorted(timeseries_data.keys())
   values = [timeseries_data[d] for d in dates]
 
-  age_group_keyset = set()
-  for v in values:
-    for k in v.get('age_groups', {}).keys():
-      age_group_keyset.add(k)
-  age_group_keys = sorted(age_group_keyset)
-
-  age_group_data = {}
-  for a in age_group_keys:
-    age_group_data[a] = []
-    for d in dates:
-      age_group_data[a].append(timeseries_data[d].get('age_groups', {}).get(a, 0))
-
-  source_keyset = set()
-  for v in values:
-    for k in v.get('sources', {}).keys():
-      source_keyset.add(k)
-  source_keys = sorted(source_keyset)
-
-  source_data = {}
-  for s in source_keys:
-    source_data[s] = [timeseries_data[d].get('sources', {}).get(s, 0) for d in dates]
+  age_group_data = munge_data_to_output(timeseries_data, dates, 'age_groups')
+  source_data = munge_data_to_output(timeseries_data, dates, 'sources')
 
   formatted_data = {
     'timeseries_dates': dates,
@@ -42,25 +25,22 @@ def main():
       'confirmed': [timeseries_data[d]['confirmed'] for d in dates],
       'tested': [timeseries_data[d]['tested'] for d in dates],
     },
-    'age_groups': {
-      'keys': age_group_keys,
-      'subseries': age_group_data,
-    },
-    'sources': {
-      'keys': source_keys,
-      'subseries': source_data,
-    }
+    'age_groups': age_group_data,
+    'sources': source_data,
   }
 
   with open('by_state/nsw.json', 'w') as f:
     json.dump(formatted_data, f, indent=2)
 
-def get_timeseries_data(nsw_feed):
-  timeseries_data = {}
+def get_timeseries_data(url):
+  post_list_soup = bs4.BeautifulSoup(requests.get(url).text, 'html.parser')
 
-  for entry in nsw_feed['entries']:
-    if ('COVID-19' in entry['title'] or 'coronavirus' in entry['title']) and 'stat' in entry['title']:
-      href = entry['links'][0]['href']
+  timeseries_data = {}
+  for li in post_list_soup.select('div#ContentHtml1Zone2 li li'):
+    # There are other media releases in the list - we only care about those that
+    # talk about COVID-19/coronavirus statistics
+    if ('COVID-19' in li.text or 'coronavirus' in li.text) and 'stat' in li.text:
+      href = li.select_one('a').attrs['href']
       soup = bs4.BeautifulSoup(requests.get(href).text, 'html.parser')
 
       date = datetime.datetime.strptime(soup.select_one('div.newsdate').text.strip(), '%d %B %Y')
@@ -106,14 +86,19 @@ def get_timeseries_data(nsw_feed):
   return timeseries_data
 
 def process_overall_table(table):
-  confirmed = [r[1] for r in table['data'] if 'Confirmed' in r[0]][0]
+  confirmed = [r[1] for r in table['data'] if 'confirmed' in r[0].lower()][0]
 
   potential_in_progress = [r[1] for r in table['data'] if 'investigation' in r[0]]
   if len(potential_in_progress) > 0:
     in_progress = potential_in_progress[0]
   else:
     in_progress = 0
-  total = [r[1] for r in table['data'] if 'Total' in r[0]][0]
+
+  # If there's a handy "total" row, use that
+  if len([r[1] for r in table['data'] if 'Total' in r[0]]) > 0:
+    total = [r[1] for r in table['data'] if 'Total' in r[0]][0]
+  else:
+    total = sum([r[1] for r in table['data']])
 
   return [confirmed, total - in_progress]
 
@@ -170,6 +155,8 @@ def parse_table(to_parse):
 
 def clean_text(text):
   text = text.replace(u'\u200b', '')
+  text = text.replace('(see', '')
+  text = text.replace('below)', '')
   return text.split('*')[0].strip()
 
 def parse_datum(datum):
@@ -177,6 +164,328 @@ def parse_datum(datum):
     return int(datum.replace(',', ''))
   else:
     return datum
+
+def add_manual_data(timeseries_data):
+
+
+  events = {
+    # https://www.health.nsw.gov.au/news/Pages/20200125_02.aspx,
+    # https://www.health.nsw.gov.au/news/Pages/20200125_03.aspx
+    '2020-01-25': {
+      'age_groups': {
+        '30-39': 1,
+        '40-49': 1,
+        '50-59': 1,
+      },
+      'sources': {
+        'Overseas acquired': 3,
+      }
+    },
+    '2020-01-26': {}, # https://www.health.nsw.gov.au/news/Pages/20200126_03.aspx
+    # https://www.health.nsw.gov.au/news/Pages/20200127_03.aspx
+    '2020-01-27': {
+      'age_groups': {
+        '20-29': 1, # Age later confirmed in https://www.health.nsw.gov.au/news/Pages/20200130_02.aspx
+      },
+      'sources': {
+        'Overseas acquired': 4, # I assume this case was also overseas acquired? Can't find hard evidence of that though
+      }
+    },
+    '2020-01-28': {}, # https://www.health.nsw.gov.au/news/Pages/20200128_01.aspx
+    '2020-01-29': {}, # https://www.health.nsw.gov.au/news/Pages/20200129_00.aspx
+    '2020-01-30': {}, # https://www.health.nsw.gov.au/news/Pages/20200130_02.aspx
+    '2020-01-31': {}, # https://www.health.nsw.gov.au/news/Pages/20200131_02.aspx
+    '2020-02-01': {}, # https://www.health.nsw.gov.au/news/Pages/20200201_00.aspx
+    '2020-02-02': {}, # https://www.health.nsw.gov.au/news/Pages/20200202_02.aspx
+    '2020-02-03': {}, # https://www.health.nsw.gov.au/news/Pages/20200203_01.aspx
+    '2020-02-04': {}, # https://www.health.nsw.gov.au/news/Pages/20200204_02.aspx
+    '2020-02-05': {}, # https://www.health.nsw.gov.au/news/Pages/20200205_01.aspx
+    '2020-02-06': {}, # https://www.health.nsw.gov.au/news/Pages/20200206_01.aspx
+    '2020-02-07': {}, # https://www.health.nsw.gov.au/news/Pages/20200207_01.aspx
+    '2020-02-08': {}, # https://www.health.nsw.gov.au/news/Pages/20200208_00.aspx
+    '2020-02-09': {}, # https://www.health.nsw.gov.au/news/Pages/20200209_00.aspx
+    '2020-02-10': {}, # https://www.health.nsw.gov.au/news/Pages/20200210_03.aspx
+    '2020-02-11': {}, # https://www.health.nsw.gov.au/news/Pages/20200211_00.aspx
+    '2020-02-12': {}, # https://www.health.nsw.gov.au/news/Pages/20200212_00.aspx
+    '2020-02-13': {}, # https://www.health.nsw.gov.au/news/Pages/20200213_01.aspx
+    '2020-02-14': {}, # https://www.health.nsw.gov.au/news/Pages/20200214_01.aspx
+    '2020-02-15': {}, # No media release this day
+    '2020-02-16': {}, # https://www.health.nsw.gov.au/news/Pages/20200216_00.aspx
+    '2020-02-17': {}, # https://www.health.nsw.gov.au/news/Pages/20200217_00.aspx
+    '2020-02-18': {}, # https://www.health.nsw.gov.au/news/Pages/20200218_00.aspx
+    '2020-02-19': {}, # https://www.health.nsw.gov.au/news/Pages/20200219_02.aspx
+    '2020-02-20': {}, # https://www.health.nsw.gov.au/news/Pages/20200220_00.aspx
+    '2020-02-21': {}, # https://www.health.nsw.gov.au/news/Pages/20200221_00.aspx
+    '2020-02-22': {}, # https://www.health.nsw.gov.au/news/Pages/20200222_01.aspx
+    '2020-02-23': {}, # https://www.health.nsw.gov.au/news/Pages/20200223_00.aspx
+    '2020-02-24': {}, # https://www.health.nsw.gov.au/news/Pages/20200224_00.aspx
+    '2020-02-25': {}, # https://www.health.nsw.gov.au/news/Pages/20200225_00.aspx
+    '2020-02-26': {}, # https://www.health.nsw.gov.au/news/Pages/20200226_00.aspx
+    '2020-02-27': {}, # No media release this day
+    '2020-02-28': {}, # No media release this day
+    '2020-02-29': {}, # No media release this day
+    # https://www.health.nsw.gov.au/news/Pages/20200301_00.aspx
+    # https://www.health.nsw.gov.au/news/Pages/20200301_01.aspx
+    '2020-03-01': {
+      'age_groups': {
+        '40-49': 1,
+        '50-59': 1,
+      },
+      'sources': {
+        'Overseas acquired': 2,
+      }
+    },
+    # This media release is date the 3rd of March, but the way the subsequent
+    # one is written suggests that this should have been on the 2nd
+    # https://www.health.nsw.gov.au/news/Pages/20200303_00.aspx
+    '2020-03-02': {
+      'age_groups': {
+        '30-39': 1,
+        '40-49': 1,
+        '50-59': 1,
+      },
+      'sources': {
+        'Overseas acquired': 1,
+        'Locally acquired - contact of a confirmed case': 1,
+        'Locally acquired - contact not identified': 1,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200303_04.aspx
+    '2020-03-03': {
+      'age_groups': {
+        '30-39': 2,
+        '50-59': 2,
+        '60-69': 2,
+      },
+      'sources': {
+        'Overseas acquired': 5,
+        'Locally acquired - contact not identified': 1,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200304_06.aspx
+    '2020-03-04': {
+      'age_groups': {
+        '20-29': 1, # Doctor who works at Liverpool hospital (age confirmed the day aften)
+        '30-39': 1, # Lives on the Northern Beaches, returned from Iran (travel history confirmed the day after)
+        '50-59': 1, # Lives in Cronulla, nurse at Dorothy Henderson Lodge
+        '60-69': 1, # Returned from Philippines
+        '70-79': 1, # Resident of Dorothy Henderson Lodge
+        '90-99': 1, # Resident of Dorothy Henderson Lodge
+      },
+      'sources': {
+        'Overseas acquired': 2,
+        'Locally acquired - contact of a confirmed case': 4, # Counting the Dorothy Henderson Lodge as epi links, and
+                       # the female doctor an epi link of the other doctor,
+                       # since they attended the same conference
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200305_01.aspx
+    # https://www.health.nsw.gov.au/news/Pages/20200305_02.aspx
+    '2020-03-05': {
+      'age_groups': {
+        # Missing age for traveler returned from Singapore, went to Goulburn, went to Darwin
+        '10-19': 1, # Mother worked at Ryde hospital
+        '50-59': 1, # Traveller returned from Italy
+        '90-99': 1, # Resident of Dorothy Henderson Lodge
+      },
+      'sources': {
+        'Overseas acquired': 2,
+        'Locally acquired - contact of a confirmed case': 2,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200306_00.aspx
+    '2020-03-06': {
+      'age_groups': {
+        '10-19': 1, # Household contact of a case in Western Sydney
+        '20-29': 2, # Two staff members at Dorothy Henderson Lodge
+      },
+      'sources': {
+        'Locally acquired - contact of a confirmed case': 3,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200307_00.aspx
+    '2020-03-07': {
+      'age_groups': {
+        '20-29': 1, # Close contact of confirmed case
+        '40-49': 3, # Two family members of confirmed case, close contact of confirmed case
+        '50-59': 1, # Close contact of confirmed case
+        '70-79': 1, # Returned from Italy
+      },
+      'sources': {
+        'Overseas acquired': 1,
+        'Locally acquired - contact of a confirmed case': 5,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200308_00.aspx
+    # https://www.health.nsw.gov.au/news/Pages/20200308_01.aspx
+    '2020-03-08': {
+      'age_groups': {
+        '30-39': 1, # Health care worker at Ryde, contact of Dorothy Henderson case
+        '40-49': 1, # Known to have travelled overseas recently
+        '50-59': 1, # Contact of confirmed case
+        '70-79': 1, # Under investigation
+      },
+      'sources': {
+        'Overseas acquired': 1,
+        'Locally acquired - contact of a confirmed case': 2,
+        'Under investigation': 1,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200309_01.aspx
+    '2020-03-09': {
+      'age_groups': {
+        '10-19': 3, # Two students at St Patrick's Marist College, Dundas (epi links to other cases);
+                    # Year 7 Willoughby Girls student, under investigation
+        '30-39': 1, # Returned from Philippines
+        '50-59': 3, # Fathers of the two Marist students (epi links to other cases);
+                    # Mother of the Willoughby student (counting as epi link to daughter)
+      },
+      'sources': {
+        'Overseas acquired': 1,
+        'Locally acquired - contact of a confirmed case': 5,
+        'Under investigation': 1,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200310_00.aspx
+    # https://www.health.nsw.gov.au/news/Pages/20200310_01.aspx
+    '2020-03-10': {
+      'age_groups': {
+        '20-29': 2, # Contact of Ryde Hospital case; returned from Hong Kong
+        '30-39': 2, # Under investigation; contact of Dorothy Henderson Lodge case
+        '40-49': 4, # Returned from South Korea; under investigation; related to Dorothy Henderson Lodge case;
+                    # Under investigation
+        '50-59': 2, # Returned from the US, another returned from US
+        '60-69': 2, # Returned from Switzerland; close contact
+        '70-79': 1, # Under investigation
+        '80-89': 1, # Dorothy Henderson Lodge resident
+      },
+      'sources': {
+        'Overseas acquired': 5,
+        'Locally acquired - contact of a confirmed case': 5,
+        'Under investigation': 4,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200311_00.aspx
+    '2020-03-11': {
+      'age_groups': {
+        '20-29': 3, # Three returned from Italy together
+        '70-79': 1, # Dorothy Henderson Lodge contact
+      },
+      'sources': {
+        'Overseas acquired': 3,
+        'Locally acquired - contact of a confirmed case': 1,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200312_00.aspx
+    '2020-03-12': {
+      'age_groups': {
+        '10-19': 2, # Close contact of Ryde case; no overseas travel
+        '20-29': 2, # No overseas travel; returned from Malaysia
+        '30-39': 2, # Under investigation; under investigation
+        '40-49': 1, # No overseas travel
+        '50-59': 2, # Returned from UK via Dubai; under investigation
+        '60-69': 3, # Two returned from Italy; under investigation
+        '80-89': 1, # No overseas travel
+      },
+      'sources': {
+        'Overseas acquired': 4,
+        'Locally acquired - contact of a confirmed case': 1,
+        'Locally acquired - contact not identified': 4,
+        'Under investigation': 4,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200313_00.aspx
+    '2020-03-13': {
+      'age_groups': {
+        '20-29': 3, # Returned from UK via Perth; returned from Switzerland via Dubai; under investigation
+        '30-39': 4, # Under investigation; under investigation; returned from US; two returned from Philippines
+        '40-49': 4, # Known contact; returned from US; community; under investigation
+        '50-59': 2, # Returned from Italy; under investigation
+      },
+      'sources': {
+        'Overseas acquired': 7,
+        'Locally acquired - contact of a confirmed case': 1,
+        'Locally acquired - contact not identified': 1,
+        'Under investigation': 5,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200314_00.aspx
+    '2020-03-14': {
+      'age_groups': {
+        '20-29': 2, # Investigation; Investigation
+        '30-39': 7, # Investigation; Investigation; Investigation; Investigation;
+                    # returned from Finland and UK; Investigation; Investigation
+        '40-49': 4, # Investigation; investigation; Investigation; returned from France and UK
+        '50-59': 1, # Investigation
+        '60-69': 6, # Returned from Germany; returned from Italy; Investigation; two returned from Italy; returned from US
+      },
+      'sources': {
+        'Overseas acquired': 7,
+        'Locally acquired - contact of a confirmed case': 0,
+        'Locally acquired - contact not identified': 0,
+        'Under investigation': 13,
+      }
+    },
+    # https://www.health.nsw.gov.au/news/Pages/20200315_00.aspx
+    '2020-03-15': {
+      'age_groups': {
+        '10-19': 2, # Investigation; investigation
+        '20-29': 4, # Investigation; investigation; close contact; investigation
+        '30-39': 5, # close contact; close contact; returned from US; investigation; investigation
+        '40-49': 5, # close contact; investigation; investigation; investigation; returned from US
+        '50-59': 3, # returned from US; returned from US; returned from Philippines
+        '60-69': 3, # returned from SG; investigation; close contact
+      },
+      'sources': {
+        'Overseas acquired': 6,
+        'Locally acquired - contact of a confirmed case': 5,
+        'Locally acquired - contact not identified': 0,
+        'Under investigation': 11,
+      }
+    },
+
+  }
+
+  age_groups = collections.defaultdict(lambda: 0)
+  sources = collections.defaultdict(lambda: 0)
+  for date in sorted(events.keys()):
+    event_data = events[date]
+    for k, v in event_data.get('age_groups', {}).iteritems():
+      age_groups[k] += v
+    for k, v in event_data.get('sources', {}).iteritems():
+      sources[k] += v
+
+    # If there's no date in the timeseries data, there's no data for that day,
+    # and it probably doesn't super matter?
+    if date in timeseries_data:
+      timeseries_data[date]['age_groups'] = copy.deepcopy(age_groups)
+      timeseries_data[date]['sources'] = copy.deepcopy(sources)
+
+  return timeseries_data
+
+def munge_data_to_output(timeseries_data, dates, data_key):
+  dates = sorted(timeseries_data.keys())
+  values = [timeseries_data[d] for d in dates]
+
+  # Generate a list of all keys for the given data series
+  # There's probably a way to do this with a Python one liner, but I think this
+  # is clearer
+  keyset = set()
+  for v in values:
+    for k in v.get(data_key, {}).keys():
+      keyset.add(k)
+  keys = sorted(keyset)
+
+  munged_data = {}
+  for k in keys:
+    munged_data[k] = []
+    for d in dates:
+      munged_data[k].append(timeseries_data[d].get(data_key, {}).get(k, 0))
+
+  return {
+    'keys': keys,
+    'subseries': munged_data,
+  }
 
 if __name__ == '__main__':
   main()
