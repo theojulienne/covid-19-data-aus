86ed00e6c9704b3b99f53ba36dfe8dcb00b91935
Theo Julienne <theojulienne@github.com>
2020-03-25 11:14:33 +1100

    Add status and tracing caching for QLD so we start collecting data from now.

---
+++ scripts.hourly/50-qld.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+
+import datetime
+import re
+import os
+
+import requests
+
+# unfortunately QLD doesn't have a history of testing data. so instead, every poll, we check this page, and save it as the "status as at" date :'(
+status_url = 'https://www.qld.gov.au/health/conditions/health-alerts/coronavirus-covid-19/current-status/current-status-and-contact-tracing-alerts'
+response_body = requests.get(status_url).text
+matches = re.findall(r'Status as at ([^<]*)', response_body)
+if len(matches) == 1:
+    status_date = matches[0]
+    parsed_date = datetime.datetime.strptime(status_date, '%d %B %Y').date().isoformat()
+    
+    status_file = 'data_cache/qld/status-tracing/' + parsed_date + '.html'
+    with open(status_file, 'wb') as f:
+        f.write(response_body.encode('utf-8'))
